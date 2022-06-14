32362d181a07a9d721d9438f296fb5dc445fc849
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-06-14 14:09:40 -0300

    feature: add product page

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,10 +1,132 @@
-// import { Header } from "../../components/Header";
+import { useEffect, useState } from "react";
+import { Cta } from "../../components/Cta";
+import { Categories } from "../../components/Categories";
+import { ProductCard } from "../../components/ProductCard";
+import { api } from "../../services/api";
+import {
+  Content,
+  Line,
+  ListContainer,
+  ListProductsContent,
+  SearchContainer,
+  SearchInput,
+  Title,
+} from "./styles";
+import { ErrorContainer } from "../../components/ProductsCaroulsel/styles";
+
+import ctaImg from "../../assets/cta-2.png";
+import searchImg from "../../assets/search.svg";
+import { Pagination } from "../../components/Pagination";
+import { Loading } from "../../components/Loading";
+
+interface ProductsProps {
+  _id: string;
+  price: number;
+  images: string[];
+  name: string;
+}
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+}
 
 export function Products() {
+  const [products, setProducts] = useState<ProductsProps[]>([]);
+  const [activeCategory, setActiveCategory] = useState(
+    "6255e1d8aedfe58bbf1b8c31"
+  );
+  const [searchProduct, setSearchProduct] = useState("");
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [pagination, setPagination] = useState<PaginationProps>(
+    {} as PaginationProps
+  );
+
+  useEffect(() => {
+    setLoading(true);
+    api
+      .get(`products?category=${activeCategory}&limit=12&page=${page}`)
+      .then((response) => {
+        setPagination({
+          totalPages: response.data.totalPages,
+          currentPage: response.data.currentPage,
+        });
+        setProducts(response.data.products);
+        setLoading(false);
+      });
+  }, [activeCategory, page]);
+
+  function handleSearch() {
+    setLoading(true);
+    api.get(`products?name=${searchProduct}&limit=12`).then((response) => {
+      setProducts(response.data.products);
+      setLoading(false);
+    });
+  }
+
   return (
-    <>
-      {/* <Header /> */}
-      <h1>Products</h1>
-    </>
+    <main>
+      <Cta
+        title="Os melhores produtos para você"
+        subtitle="Solte sua imaginação ao criar novos produtos"
+        paragraph="Que tal trabalhar com algo novo ?
+      Aqui você encontra o que há de mais novo no mercdo, para vocẽ poder inovar"
+        reverse={false}
+        image={ctaImg}
+      />
+
+      <Content>
+        <Line />
+
+        <Title>Categorias</Title>
+
+        <SearchContainer>
+          <SearchInput
+            type="text"
+            value={searchProduct}
+            placeholder="Buscar produtos"
+            onChange={(event) => setSearchProduct(event.target.value)}
+          />
+          <button onClick={handleSearch}>
+            <img src={searchImg} alt="Buscar" />
+          </button>
+        </SearchContainer>
+
+        <Categories
+          setActiveCategory={setActiveCategory}
+          selectedCategory={activeCategory}
+        />
+
+        {loading &&  <Loading />}
+
+        {products.length ? (
+          <ListContainer>
+            <ListProductsContent>
+              {products.map((product) => (
+                <ProductCard
+                  _id={product._id}
+                  images={product.images}
+                  name={product.name}
+                  price={product.price}
+                  key={product._id}
+                />
+              ))}
+            </ListProductsContent>
+            <Pagination
+              setPage={setPage}
+              currentPage={pagination.currentPage}
+              totalPages={pagination.totalPages}
+            />
+          </ListContainer>
+        ) : (
+          <ErrorContainer>
+            <span>
+              No momento não existe produtos cadastrados para essa categoria
+            </span>
+          </ErrorContainer>
+        )}
+      </Content>
+    </main>
   );
 }
---
+++ src/pages/Products/styles.ts
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 2.5rem auto;
+
+  display: grid;
+  grid-template-columns: 240px 1fr;
+  grid-template-areas:
+    "line line"
+    "title input"
+    "category products";
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  height: 2px;
+  background: var(--line);
+  grid-area: line;
+`;
+
+export const Title = styled.h3`
+  grid-area: title;
+
+  margin-top: 2.5rem;
+  margin-bottom: 1.5rem;
+
+  color: var(--text-body);
+
+  font-size: 1.5rem;
+  margin-left: 1rem;
+`;
+
+export const SearchInput = styled.input`
+  grid-area: input;
+  width: calc(100% - 1.25rem);
+  height: 3.5rem;
+  padding: 1.25rem;
+
+  margin-top: 2.5rem;
+  margin-left: 1.25rem;
+
+  border: none;
+  border: 1px solid var(--line);
+  border-radius: 1.5rem;
+
+  box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.1);
+  color: var(--text-body);
+  font-size: 1rem;
+
+  &::placeholder {
+    color: var(--text-paragraph);
+    font-weight: 500;
+    font-size: 1rem;
+  }
+`;
+
+export const SearchContainer = styled.div`
+  position: relative;
+
+  button {
+    background: none;
+    border: none;
+    position: absolute;
+    right: 0;
+    top: 3.4375rem;
+    margin-right: 1rem;
+  }
+`;
+
+export const ListContainer = styled.div`
+  grid-area: products;
+
+  margin-top: 3.375rem;
+  margin-left: 3.75rem;
+`;
+
+export const ListProductsContent = styled.div`
+  display: grid;
+  grid-gap: 2.5rem;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: auto;
+`;
