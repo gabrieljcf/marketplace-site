228641637f2a41e1251f0cc642bbdb255edfbc8b
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-06-14 14:07:00 -0300

    feature: add loading

--- src/components/OurProducts/index.tsx
+++ src/components/OurProducts/index.tsx
@@ -4,6 +4,7 @@ import { api } from "../../services/api";
 import { List } from "../List";
 
 import { Container } from "./style";
+import { Loading } from "../Loading";
 
 interface Categories {
   _id: string;
@@ -20,22 +21,33 @@ interface Products {
 
 export function OurProducts() {
   const [categories, setCategories] = useState<Categories[]>([]);
-  const [activeCategory, setActiveCategory] = useState<string>("6255e1d8aedfe58bbf1b8c31");
+  const [activeCategory, setActiveCategory] = useState<string>(
+    "6255e1d8aedfe58bbf1b8c31"
+  );
   const [products, setProducts] = useState<Products[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    api.get("categories")
-      .then(response => setCategories(response.data));
+    setLoading(true);
+    api.get("categories").then((response) => {
+      setCategories(response.data);
+      setLoading(false);
+    });
   }, []);
 
   useEffect(() => {
-    api.get(`products?category=${activeCategory}`)
-      .then(response => setProducts(response.data.products));
+    setLoading(true);
+    api.get(`products?category=${activeCategory}`).then((response) => {
+      setProducts(response.data.products);
+      setLoading(false);
+    });
   }, [activeCategory]);
 
   return (
     <Container>
-      <h2>Nossos <span>Produtos</span></h2>
+      <h2>
+        Nossos <span>Produtos</span>
+      </h2>
 
       <List
         items={categories}
@@ -43,11 +55,11 @@ export function OurProducts() {
         activeItem={activeCategory}
       />
 
-      <ProductsCarousel 
+      <ProductsCarousel
         products={products}
         notFoundMessage="No momento não existe produtos cadastrados para essa categoria"
       />
-
+      {loading && <Loading />}
     </Container>
   );
 }
