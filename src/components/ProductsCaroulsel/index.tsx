9270adb8ee4fdb84f93a9e4779b1c5af7770de51
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-06-14 14:08:26 -0300

    feature: add product carousel component

--- src/components/ProductsCaroulsel/index.tsx
+++ src/components/ProductsCaroulsel/index.tsx
@@ -1,10 +1,10 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper";
 import { ProductCard } from "../ProductCard";
 
 import { Container, ErrorContainer } from "./styles";
 
-import 'swiper/css';
+import "swiper/css";
 import "swiper/css/pagination";
 
 interface Products {
@@ -19,41 +19,38 @@ interface HighLigthProducsProps {
   notFoundMessage: string;
 }
 
-export function ProductsCarousel({ products, notFoundMessage }: HighLigthProducsProps) {
+export function ProductsCarousel({
+  products,
+  notFoundMessage,
+}: HighLigthProducsProps) {
   return (
     <Container>
-      {
-        products.length
-          ? <Swiper
-            modules={[Pagination, Autoplay]}
-            spaceBetween={0}
-            slidesPerView={4}
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-          >
-            {
-              products.map(product => (
-                <SwiperSlide key={product._id}>
-                  <ProductCard
-                    _id={product._id}
-                    name={product.name}
-                    images={product.images}
-                    price={product.price}
-                  />
-                </SwiperSlide>
-              ))
-            }
-          </Swiper>
-          :
-          <ErrorContainer>
-            <span>{notFoundMessage}</span>
-          </ErrorContainer>
-      }
+      {products.length ? (
+        <Swiper
+          modules={[ Autoplay]}
+          spaceBetween={0}
+          slidesPerView={4}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        >
+          {products.map((product) => (
+            <SwiperSlide key={product._id}>
+              <ProductCard
+                _id={product._id}
+                name={product.name}
+                images={product.images}
+                price={product.price}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <ErrorContainer>
+          <span>{notFoundMessage}</span>
+        </ErrorContainer>
+      )}
     </Container>
   );
 }
--- src/components/ProductsCaroulsel/styles.ts
+++ src/components/ProductsCaroulsel/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 1000px;
+  max-width: 1120px;
   margin: 0 auto;
 `
 
