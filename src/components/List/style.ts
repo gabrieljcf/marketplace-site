64bab461870b961deebb17b31b8992bb6f17d1b7
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-06-14 14:05:51 -0300

    fix: adjust list margin

--- src/components/List/style.ts
+++ src/components/List/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const Listcontainer = styled.ul`
-  margin-top: 2rem;
+  margin: 2rem 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
