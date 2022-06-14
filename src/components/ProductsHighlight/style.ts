a84c3d2da8eb1d28e274ff3141bf7894cda4ca13
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-06-14 14:09:04 -0300

    fix: adjust width

--- src/components/ProductsHighlight/style.ts
+++ src/components/ProductsHighlight/style.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  max-width: 1000px;
+  max-width: 1120px;
   margin: 4rem auto;
 
   h2 {
     text-align: center;
     font-size: 2rem;
     color: var(--text-body);
-
+    margin-bottom: 2rem;
+    
     span {
       color: var(--pink-dark);
     }
