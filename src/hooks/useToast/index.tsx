67dca0f88c643d1d29602c870ed4d362a0a98f51
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-06-15 19:29:18 -0300

    feat: add toast hook

---
+++ src/hooks/useToast/index.tsx
@@ -0,0 +1,15 @@
+import Swal from "sweetalert2";
+
+export function useToast() {
+  return Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+}
