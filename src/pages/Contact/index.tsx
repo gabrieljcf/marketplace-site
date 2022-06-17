365d2b8709933824be3f87a96e1a283d6ed96535
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-06-16 22:43:01 -0300

    feature: add contact component

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,56 @@
+import { Loading } from "../../components/Loading";
+import { Cta } from "../../components/Cta";
+
+import ctaImg from "../../assets/cta.png";
+import { ContactContainer } from "./styles";
+import { Line } from "../Products/styles";
+import { useEffect, useState } from "react";
+
 export function Contact() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <main>
+      <Cta
+        title="Os melhores produtos para você"
+        subtitle="Solte sua imaginação ao criar novos produtos"
+        paragraph="Que tal trabalhar com algo novo ?
+        Aqui você encontra o que há de mais novo no mercdo, para vocẽ poder inovar"
+        reverse={false}
+        image={ctaImg}
+        link="/products"
+        buttonText="Compre Agora"
+      />
+
+      <ContactContainer>
+        <h3>
+          Conheça nossas <span>lojas</span>
+        </h3>
+        <Line />
+
+        <div>
+          <h2>
+            Alagoinhas-<span>BA</span>
+          </h2>
+          <p>Endereço: Rua Centro 1234</p>
+          <p>Telefone: 75 3333-3435</p>
+          <p>E-mail: artefestasalagoinhas@hotmailcom</p>
+
+          <h2>
+            Catu-<span>BA</span>
+          </h2>
+          <p>Endereço: Rua Centro 1234</p>
+          <p>Telefone: 75 3333-3435</p>
+          <p>E-mail: artefestascatu@hotmailcom</p>
+        </div>
+      </ContactContainer>
+      {isLoading && <Loading />}
+    </main>
   );
 }
---
+++ src/pages/Contact/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const ContactContainer = styled.div`
+  max-width: 1120px;
+  margin: 5rem auto;
+
+  h3 {
+    font-size: 1.5rem;
+    color: var(--text-body);
+    margin-bottom: 1.5rem;
+  }
+
+  h2 {
+    margin-top: 2.5rem;
+    max-width: 470px;
+    color: var(--text-body);
+    font-size: 2rem;
+  }
+
+  span {
+    color: var(--pink-dark);
+  }
+
+  p {
+    color: var(--text-paragraph);
+    font-size: 1rem;
+    font-weight: 500;
+    margin-top: 0.3125rem;
+  }
+`;
