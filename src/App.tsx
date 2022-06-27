7a763258eda94dd8062d3f769d9d9de1c26d3a04
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-06-26 23:05:49 -0300

    feature: add basket badge provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { BasketBadgeProvider } from "./hooks/useBasketBadge";
 import { Router } from "./routes";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <>
+    <BasketBadgeProvider>
       <Header />
       <Router />
-      <Footer />                  
+      <Footer />
       <GlobalStyle />
-    </>
+    </BasketBadgeProvider>
   );
 }
