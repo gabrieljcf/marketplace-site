d1d126f9be30d915a60d156a941f885e2b2fb173
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-10-14 11:07:15 -0300

    feat: add error container

--- src/pages/Products/styles.ts
+++ src/pages/Products/styles.ts
@@ -107,3 +107,19 @@ export const ListProductsContent = styled.div`
     grid-template-columns: repeat(2, 1fr);
   }
 `;
+
+export const ErrorContainer = styled.div`
+  margin: 2.5rem 0;
+  text-align: center;
+
+  span {
+    color: var(--pink);
+    font-weight: 600;
+    font-size: 1.2rem;
+
+    @media screen and (max-width: 768px) {
+      padding: 0 1rem;
+      font-size: 1.1rem;
+    }
+  }
+`;
