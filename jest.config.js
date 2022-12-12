588d8c4e36ff98255a361515272de92ed37ffe2f
Gabriel Nogueira <gabrieljcf@hotmail.com>
2022-12-11 23:35:32 -0300

    feat: add jest-setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     testPathIgnorePatterns: ['/node_modules/'],
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],  
     transform: {
         "ˆ.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
     }
